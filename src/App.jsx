2d5e26be286bea95ca68136aa3200de7ebf018dc
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-04-21 13:09:37 +0200

    Add Logo component and update App component to use Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import Logo from "./components/Logo";
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline font-inter">Hello world!</h1>
-  );
+  return <Logo />;
 }
