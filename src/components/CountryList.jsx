c1ff5d16476c2138e7ab743f16b2c01f8989914a
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-04-23 11:03:50 +0200

    Update CountryList component to handle empty data case

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,18 +1,23 @@
 import CountryCard from "./CountryCard";
+import ShowMessage from "./ShowMessage";
 
 const CountryList = ({ data }) => {
   return (
     <div className="mt-8 grid justify-between gap-x-[70px] gap-y-12 md:mt-12 md:grid-cols-[repeat(2,minmax(0,_auto))] lg:grid-cols-[repeat(4,minmax(0,_auto))] lg:gap-y-[70px]">
-      {data.map((country) => (
-        <CountryCard
-          key={country.name.official}
-          name={country.name.common}
-          population={country.population}
-          region={country.region}
-          capital={country.capital}
-          flag={country.flags.svg}
-        />
-      ))}
+      {data && data.length ? (
+        data.map((country) => (
+          <CountryCard
+            key={country.name.official}
+            name={country.name.common}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            flag={country.flags.svg}
+          />
+        ))
+      ) : (
+        <ShowMessage message={"Empty"} />
+      )}
     </div>
   );
 };
