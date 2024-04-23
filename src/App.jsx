1a2afb5e0f16edf6586ac302fc40d44e0c974cc1
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-04-23 09:48:57 +0200

    Update ESLint configuration to disable react/prop-types rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 import Header from "./components/Header";
 import RegionMenu from "./components/RegionMenu";
 import SearchInput from "./components/SearchInput";
+import ShowMessage from "./components/ShowMessage";
 
 export default function App() {
+  const [countriesList, setCountriesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    fetchCountriesData();
+  }, []);
+  const fetchCountriesData = () => {
+    setIsLoading(true);
+
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountriesList(data);
+        // console.log(data);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
     <div className="min-h-screen w-screen bg-gray-100 font-inter dark:bg-gray-900 dark:text-gray-100">
       <Header />
       <div className="container mx-auto px-5 md:px-0">
-        <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
-          <SearchInput />
-          <RegionMenu />
-        </div>
+        {isError && <ShowMessage message="Something went wrong!" />}
+        {isLoading && <ShowMessage message="Loading countries data..." />}
+        {!isError && !isLoading && (
+          <>
+            <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
+              <SearchInput />
+              <RegionMenu />
+            </div>
+            <CountryList data={countriesList} />
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,31 @@
+const CountryCard = ({ name, population, region, capital, flag }) => {
+  return (
+    <a href="#">
+      <div className="h-full rounded bg-gray-50 p-3 shadow-md dark:bg-gray-800 lg:w-[264px] ">
+        <img
+          className="md-4 h-40 w-full rounded-md"
+          src={flag}
+          alt="flag"
+          loading="lazy"
+        />
+        <h2 className="mb-4 ml-3 text-lg font-extrabold">{name}</h2>
+        <div className="ml-3 flex flex-col gap-2">
+          <p>
+            <span className="font-semibold">Population</span>
+            <span className="font-light">{population}</span>
+          </p>
+          <p>
+            <span className="font-semibold">Region:</span>
+            <span className="font-light">{region}</span>
+          </p>
+          <p>
+            <span className="font-semibold">Capital:</span>
+            <span className="font-light">{capital}</span>
+          </p>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default CountryCard;
